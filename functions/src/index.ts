4e848a276757398fef298d193ef3409de0e51452
Sousa Essens <sousaessens@gmail.com>
2025-08-12 21:29:11 +0000

    === Deploying to 'planejei-4oo42'...
    i  deploying functions
    Running com

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,10 @@
+
 import {setGlobalOptions} from 'firebase-functions/v2';
 import * as functions from 'firebase-functions';
 import * as logger from 'firebase-functions/logger';
-import {onCall, onCallGenkit} from 'firebase-functions/v2/https';
-import type {CallableRequest} from 'firebase-functions/v2/https';
+import {onCall, type CallableRequest} from 'firebase-functions/v2/https';
 import * as admin from 'firebase-admin';
 import type {Query} from 'firebase-admin/firestore';
-import {genkit, z} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
-import {defineSecret} from 'firebase-functions/params';
-import {enableFirebaseTelemetry} from '@genkit-ai/firebase';
-
-// Genkit Initialization
-const apiKey = defineSecret('GOOGLE_GENAI_API_KEY');
-enableFirebaseTelemetry();
-const ai = genkit({
-  plugins: [googleAI()],
-});
 
 // Firebase Admin Initialization
 setGlobalOptions({maxInstances: 10});
@@ -103,37 +92,3 @@ const deleteCollection = async (collectionPath: string, batchSize: number) => {
     snapshot = await query.get();
   }
 };
-
-// Genkit Sample Flow
-const menuSuggestionFlow = ai.defineFlow(
-  {
-    name: 'menuSuggestionFlow',
-    inputSchema: z.string().describe('A restaurant theme').default('seafood'),
-    outputSchema: z.string(),
-    streamSchema: z.string(),
-  },
-  async (subject, {sendChunk}) => {
-    const prompt = `Suggest an item for the menu of a 
-  ${subject} themed restaurant`;
-    const {response, stream} = ai.generateStream({
-      model: 'googleai/gemini-1.5-flash-latest',
-      prompt: prompt,
-      config: {
-        temperature: 1,
-      },
-    });
-
-    for await (const chunk of stream) {
-      sendChunk(chunk.text);
-    }
-
-    return (await response).text;
-  }
-);
-
-export const menuSuggestion = onCallGenkit(
-  {
-    secrets: [apiKey],
-  },
-  menuSuggestionFlow
-);
