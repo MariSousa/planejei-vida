f41bb12727597afa9b84768f5cd4601e023d9901
Sousa Essens <sousaessens@gmail.com>
2025-08-12 22:24:44 +0000

    ✓ Starting... 2025-08-12T22:23:06Z [web] Error:  ⚠ Invalid next.config.m

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1754914726186.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev"],
+  devIndicators: {
+    buildActivity: false,
   },
 };
 
