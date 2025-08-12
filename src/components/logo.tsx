7c7d77e905458912c9ddf12a4171b3f539cfc119
Sousa Essens <sousaessens@gmail.com>
2025-08-12 22:42:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo>
    
    Relevant f

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@
 export function Logo() {
   return (
     <div className="flex items-center justify-center p-2 h-[58px]">
-      {/* Logo can be placed here */}
+      <h1 className="text-2xl font-bold text-primary">Planejei</h1>
     </div>
   );
 }
